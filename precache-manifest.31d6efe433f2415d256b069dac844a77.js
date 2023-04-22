8debc5e29eb4e4e74feda509cc6c9635714b2a82
Code Sandbox <hello@codesandbox.io>
2023-04-22 04:39:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.31d6efe433f2415d256b069dac844a77.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a0e8e41a25706c7a402e5b192ab23f",
+    "url": "https://jagadeeshreddiie.github.io/csb-l4qsyy/index.html"
+  },
+  {
+    "revision": "1203adf5664069b43cce",
+    "url": "https://jagadeeshreddiie.github.io/csb-l4qsyy/static/js/2.71c787fe.chunk.js"
+  },
+  {
+    "revision": "fdc72d26081f151048f9",
+    "url": "https://jagadeeshreddiie.github.io/csb-l4qsyy/static/js/main.216b4549.chunk.js"
+  },
+  {
+    "revision": "84751d577f9e9115aa22",
+    "url": "https://jagadeeshreddiie.github.io/csb-l4qsyy/static/js/runtime-main.a39f3d55.js"
+  }
+]);
